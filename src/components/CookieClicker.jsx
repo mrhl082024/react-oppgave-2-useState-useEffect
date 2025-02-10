839a15a4b5a542049b8f6935825d8f4095b62b37
mrhl082024 <mrhl082024@gmail.com>
2025-02-10 14:36:15 +0100

    changed some syntax to be more like industry standard

--- src/components/CookieClicker.jsx
+++ src/components/CookieClicker.jsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
-import cookie from "../assets/cookie.png"
-import "../styles/cookieClicker.css"
+import cookie from "../assets/cookie.png";
+import "../styles/cookieClicker.css";
 
 export default function cookieClicker() {
   const [count, setCount] = useState(0);
-  function handleIncrement(){
-    setCount(count + 1)
+  function handleIncrement() {
+    setCount((prev) => prev + 1);
   }
-  function resetIncrement(){
-    setCount(0)
+  function resetIncrement() {
+    setCount((prev) => (prev = 0));
   }
-  return <div id="cookieCard">
-  <p>{count} Cookies!</p>
-  <button id="cookieBtn" onClick={handleIncrement} ><img id="cookieImg" src={cookie} alt="cookie" /></button>
-  <button id="resetBtn" onClick={resetIncrement}>Reset Counter</button>
-  </div>;
+  return (
+    <div id="cookieCard">
+      <p>{count} Cookies!</p>
+      <button id="cookieBtn" onClick={handleIncrement}>
+        <img id="cookieImg" src={cookie} alt="cookie" />
+      </button>
+      <button id="resetBtn" onClick={resetIncrement}>
+        Reset Counter
+      </button>
+    </div>
+  );
 }
