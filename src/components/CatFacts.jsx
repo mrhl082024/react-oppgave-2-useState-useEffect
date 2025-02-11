63ebecdcb720553227e071085a8700073f63dae7
mrhl082024 <mrhl082024@gmail.com>
2025-02-11 13:03:09 +0100

    fetch logic is done

--- src/components/CatFacts.jsx
+++ src/components/CatFacts.jsx
@@ -1,21 +1,27 @@
 import { useEffect, useState } from "react";
 
-export default function CatFacts(){
-    const [data, setData] = useState(null)
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
+export default function CatFacts() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-
-
-    useEffect(()=>{
-        const fetchData = async() => {
-            setLoading(true)
-            setError(null)
-
-
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(`https://catfact.ninja/facts?limit=5`);
+        if (!response.ok) {
+          throw new Error(`HTTP Error. Status ${response.status}`);
         }
-        fetchData()
-
-    },[])
-    
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
 }
