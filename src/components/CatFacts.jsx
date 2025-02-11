190d62d0b35b7c8d3ebe8bd971c775cbec0dc1cc
mrhl082024 <mrhl082024@gmail.com>
2025-02-11 12:29:34 +0100

    started to work on the template for api fetch

--- src/components/CatFacts.jsx
+++ src/components/CatFacts.jsx
@@ -1,6 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function CatFacts(){
-    useEffect(()=>{},[])
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+
+
+    useEffect(()=>{
+        const fetchData = async() => {
+            setLoading(true)
+            setError(null)
+
+
+        }
+        fetchData()
+
+    },[])
     
 }
